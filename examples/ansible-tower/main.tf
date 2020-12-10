a50cfcf88f31ad750105260e8dd32533d84df6ae
Elijah Voigt <evoigt@cloudbolt.io>
2020-12-10 12:10:34 -0800

    [OUP-54] Ansible Tower Deployment example

---
+++ examples/ansible-tower/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    onefuse = {
+      source  = "CloudBoltSoftware/onefuse"
+      version = ">= 1.10.3"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+// Inititalize OneFuse Provider
+provider "onefuse" {
+  scheme     = "https"
+  address    = "onefuse_fqdn"
+  port       = "port"
+  user       = "admin"
+  password   = "admin"
+  verify_ssl = "false"
+}
+
+/*
+// OneFuse Ansible Tower Policy
+data "onefuse_ansible_tower_policy" "foo" {
+  name = "my-ansible-tower-policy-name"
+}
+*/
+
+resource "onefuse_ansible_tower_deployment" "bar" {
+  ansible_tower_policy_id = 1 // Refers to onefuse_ansible_tower_deployment data source to retrieve ID
+  // ansible_tower_policy_id = data.onefuse_ansible_tower_policy.id
+  workspace_url = "" // Leave blank for default workspace
+  limit = "..." // Ansible Tower Policy Limit
+  hosts = [ "host1", "host2", ] // Hosts to run the policy against
+  template_properties = {
+        property1        = "value1" // Your properties and values to pass into module
+        proeprty2        = "value2"
+  }
+}
