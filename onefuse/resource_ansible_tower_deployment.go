5a4cae13a79c844827a533f0cba6464e40785131
Elijah Voigt <evoigt@cloudbolt.io>
2020-12-10 17:48:32 -0800

    [OUP-15] Specing out terraform ansible tower resource

---
+++ onefuse/resource_ansible_tower_deployment.go
@@ -0,0 +1,142 @@
+// Copyright 2020 CloudBolt Software
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package onefuse
+
+import (
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/pkg/errors"
+)
+
+func resourceAnsibleTowerDeployment() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAnsibleTowerDeploymentCreate,
+		Read:   resourceAnsibleTowerDeploymentRead,
+		Update: resourceAnsibleTowerDeploymentUpdate,
+		Delete: resourceAnsibleTowerDeploymentDelete,
+		Schema: map[string]*schema.Schema{
+			"policy_id": {
+				Type:     schema.TypeInt,
+				Required: true,
+			},
+			"workspace_url": {
+				Type:     schema.TypeString,
+				Computed: true,
+				Optional: true,
+			},
+			"limit": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"hosts": {
+				Type: schema.TypeList,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+				Optional: true,
+			},
+			"template_properties": {
+				Type:     schema.TypeMap,
+				Optional: true,
+			},
+		},
+	}
+}
+
+func bindAnsibleTowerDeploymentResource(d *schema.ResourceData, ansibleDeployment *AnsibleTowerDeployment) error {
+	log.Println("onefuse.bindAnsibleTowerDeploymentResource")
+
+	if err := d.Set("workspace_url", ansibleDeployment.Links.Workspace.Href); err != nil {
+		return errors.WithMessage(err, "Cannot set workspace: "+ansibleDeployment.Links.Workspace.Href)
+	}
+
+	if err := d.Set("hosts", ansibleDeployment.Hosts); err != nil {
+		return errors.WithMessage(err, "Cannot set hosts: "+ansibleDeployment.Hosts)
+	}
+
+	if err := d.Set("limit", ansibleDeployment.Limit); err != nil {
+		return errors.WithMessage(err, "Cannot set limit: "+ansibleDeployment.Limit)
+	}
+
+	ansibleTowerPolicyURLSplit := strings.Split(ansibleDeployment.Links.Policy.Href, "/")
+	ansibleTowerPolicyID := ansibleTowerPolicyURLSplit[len(ansibleTowerPolicyURLSplit)-2]
+	ansibleTowerPolicyIDInt, _ := strconv.Atoi(ansibleTowerPolicyID)
+	if err := d.Set("policy_id", ansibleTowerPolicyIDInt); err != nil {
+		return errors.WithMessage(err, "Cannot set policy")
+	}
+
+	return nil
+}
+
+func resourceAnsibleTowerDeploymentCreate(d *schema.ResourceData, m interface{}) error {
+	log.Println("onefuse.resourceAnsibleTowerDeploymentCreate")
+
+	var hosts []string
+	for _, group := range d.Get("hosts").([]interface{}) {
+		hosts = append(hosts, group.(string))
+	}
+
+	config := m.(Config)
+
+	newAnsibleTowerDeployment := AnsibleTowerDeployment{
+		PolicyID:           d.Get("policy_id").(int),
+		WorkspaceURL:       d.Get("workspace_url").(string),
+		DNSSuffix:          d.Get("limit").(string),
+		Hosts:              hosts,
+		TemplateProperties: d.Get("template_properties").(map[string]interface{}),
+	}
+
+	ansibleDeployment, err := config.NewOneFuseApiClient().CreateAnsibleTowerDeployment(&newAnsibleTowerDeployment)
+	if err != nil {
+		return err
+	}
+	d.SetId(strconv.Itoa(ansibleDeployment.ID))
+
+	return bindAnsibleTowerDeploymentResource(d, ansibleDeployment)
+}
+
+func resourceAnsibleTowerDeploymentRead(d *schema.ResourceData, m interface{}) error {
+	log.Println("onefuse.resourceAnsibleTowerDeploymentRead")
+
+	config := m.(Config)
+
+	id := d.Id()
+	intID, err := strconv.Atoi(id)
+	if err != nil {
+		return err
+	}
+
+	ansibleDeployment, err := config.NewOneFuseApiClient().GetAnsibleTowerDeployment(intID)
+	if err != nil {
+		return err
+	}
+
+	return bindAnsibleTowerDeploymentResource(d, ansibleDeployment)
+}
+
+func resourceAnsibleTowerDeploymentUpdate(d *schema.ResourceData, m interface{}) error {
+	log.Println("onefuse.resourceAnsibleTowerDeploymentUpdate")
+	log.Println("No Op!")
+}
+
+func resourceAnsibleTowerDeploymentDelete(d *schema.ResourceData, m interface{}) error {
+	log.Println("onefuse.resourceAnsibleTowerDeploymentDelete")
+
+	config := m.(Config)
+
+	id := d.Id()
+	intID, err := strconv.Atoi(id)
+	if err != nil {
+		return err
+	}
+
+	return config.NewOneFuseApiClient().DeleteAnsibleTowerDeployment(intID)
+}
